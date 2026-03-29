3a20d98ca9cb9f8f27467f18f5310554a03e684b
Advaith <Advaith.Apparasu865@education.nsw.gov.au>
2026-03-29 12:48:04 +1100

    data types

---
+++ Lesson2/activity1.py
@@ -0,0 +1,10 @@
+name = "Advaith"
+age = 13
+height = 150.8
+is_student = True
+print("name of person", name, type(name))
+print("age of person", age, type(age))
+print("height of person", height, type(height))
+print("student", is_student, type(is_student))
+print(int(height))
+print(bool(age))
