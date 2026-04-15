71690199713c48d9b388818db5a1b361c3b285e3
Advaith <Advaith.Apparasu865@education.nsw.gov.au>
2026-04-15 12:25:12 +1000

    armstrong number

---
+++ Lesson4/armstrong.py
@@ -0,0 +1,12 @@
+num = int(input("enter the number "))
+temp = num
+sum = 0
+while temp >0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if num == sum:
+    print("armstrong number")
+else:
+    print("not an armstrong number")
